1a414011629ae0e681e5d51ea0bea78c982da68d
Yaseen KC <ymkcmpd@gmail.com>
2024-09-13 04:12:28 +0530

    converted to jsx

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello World"),
-    React.createElement("h2", {}, "I'am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'am an h1 tag"),
-    React.createElement("h2", {}, "I'am an h2 tag"),
-  ]),
-]);
-
+// const parent = React.createElement("h1", { id: "heading" }, "Namaste React");
+const parent = <h1 id="heading">Namaste React using JSX</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(parent);
