6a46e0498989e11a7766ddf456d9236ed7ad915e
Yaseen KC <ymkcmpd@gmail.com>
2024-09-12 12:53:57 +0530

    add episode 2 igniting the app

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'am an h1 tag"),
+    React.createElement("h1", {}, "Hello World"),
     React.createElement("h2", {}, "I'am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
