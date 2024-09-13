8d1279103f21aee5b273d7bd0219a13ea5c7b367
Yaseen KC <ymkcmpd@gmail.com>
2024-09-14 02:05:16 +0530

    add:function component

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // const parent = React.createElement("h1", { id: "heading" }, "Namaste React");
-const parent = <h1 id="heading">Namaste React using JSX</h1>;
+const Parent = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
+
+// ! Functional Component - Function that return JSX or ReactElement
+const HeadingComponent = () => (
+  <div id="container">
+    <Parent />
+    <h1>Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
