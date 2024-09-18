29c523ee8c62c5958c00f5f04abd91bb6b1165d5
Yaseen KC <ymkcmpd@gmail.com>
2024-09-18 13:12:05 +0530

    StateHook - useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/resList";
 
-
 const Body = () => {
+  const [resListState, setResList] = useState(resList);
+
+  const filterTopRated = () => {
+    const filteredList = resListState.filter((res) => res.avgRating >= 4.6);
+    setResList(filteredList);
+  };
+
   return (
     <div className="body">
       <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={filterTopRated}>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {/* <RestaurantCard resData={resList[0]} />    */}
-        {resList.map((restaurant) => (
+        {resListState.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
     </div>
   );
-  ``;
 };
 
 export default Body;
