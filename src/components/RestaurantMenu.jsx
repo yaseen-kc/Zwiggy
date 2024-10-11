6f3a8ae5bf4eca29c53f92ce7ceb3972c23fee5e
Yaseen KC <ymkcmpd@gmail.com>
2024-10-11 22:59:18 +0530

    .

---
+++ src/components/RestaurantMenu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RestaurantMenu = () => {
+  return (
+    <div>
+      <h1>Restaurant ID : 123</h1>
+      <h2>Restaurant Name</h2>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
