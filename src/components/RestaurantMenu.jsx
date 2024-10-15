c7df33658c9fb429f92b03824107a01c0a90461e
Yaseen KC <ymkcmpd@gmail.com>
2024-10-15 23:09:03 +0530

    add: fetch resID from params

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,10 +1,64 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(MENU_API + resId);
+    // const data = await fetch(
+    //   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=9.968199&lng=76.3182346&restaurantId=704921&catalog_qa=undefined&submitAction=ENTER"
+    // );
+    const json = await data.json();
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) return <Shimmer />;
+
+  const {
+    name,
+    cuisines,
+    costForTwoMessage,
+    locality,
+    avgRating,
+    totalRatingsString,
+    sla,
+  } = resInfo?.cards[2]?.card?.card?.info;
+
+  const cards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+
+  let itemCards = cards.find((c) => c?.card?.card?.itemCards)?.card?.card
+    ?.itemCards;
+
+  // console.log(itemCards);
   return (
-    <div>
-      <h1>Restaurant ID : 123</h1>
-      <h2>Restaurant Name</h2>
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      <p>{locality}</p>
+      <h2>⭐{avgRating}</h2>
+      <h4>{totalRatingsString}</h4>
+      <p>💵{costForTwoMessage}</p>
+      <h4>{sla.deliveryTime} Minutes</h4>
+      <h2>Menu</h2>
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - {" Rs."}
+            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
