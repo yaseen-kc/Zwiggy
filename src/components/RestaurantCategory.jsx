74f54307abb2481d487ba5eaf249babaf05fe882
Yaseen KC <ymkcmpd@gmail.com>
2024-11-06 06:07:11 +0530

    feat:Lifting State in react

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,16 +1,19 @@
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data }) => {
+const RestaurantCategory = ({ data,showItems,setShowIndex }) => {
+  const handleClick=()=>{
+    setShowIndex()
+  }
   return (
     <div>
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 rounded-md">
-        <div className="flex justify-between">
+        <div className="flex justify-between cursor-pointer" onClick={handleClick}>
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
           </span>
           <span>▼</span>
         </div>
-        <ItemList items={data.itemCards} />
+        {showItems&&<ItemList items={data.itemCards} />}
       </div>
     </div>
   );
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -2,11 +2,14 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const resInfo = useRestaurantMenu(resId);
+
+const [showIndex,setShowIndex]=useState(null)
 
   if (resInfo === null) return <Shimmer />;
 
@@ -31,8 +34,12 @@ const RestaurantMenu = () => {
       <p className="font-bold text-lg">
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
-      {categories.map((category) => (
-        <RestaurantCategory data={category.card.card} />
+      {categories.map((category,index) => (
+        <RestaurantCategory 
+        key={category?.card?.card?.title}
+        data={category?.card?.card} 
+        showItems={index === showIndex ? true:false}
+        setShowIndex={()=> setShowIndex(index)}/>
       ))}
     </div>
   );
